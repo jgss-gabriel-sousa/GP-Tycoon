c48c3d4bea0ed6e1ed17211fc201436cf1761150
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2024-06-03 02:07:48 -0300

    feat - sponsors

--- ui/viewSponsors.js
+++ ui/viewSponsors.js
@@ -1,180 +1,108 @@
-import { accentsTidy, NumberF } from "../scripts/utils.js";
-import { countryCodes } from "../data/countryCodes.js";
 import { game } from "../scripts/game.js";
-import { market } from "./market.js";
-import { getSalary } from "../scripts/drivers.js";
 import { genTeamHTML } from "../scripts/main.js";
-import { publishNews } from "./news.js";
+import { NumberF } from "../scripts/utils.js";
 
 export function viewSponsors(){
     let html = "";
     const team = game.teams[game.team];
+    
+    for(let i = 0; i < team.driversAcademy.length; i++){
+        if(game.drivers[driverName].team != team.name){
+            team.driversAcademy.splice(i, 1);
+        }
+    }
+    
+    const totalSponsorsValue = (team.sponsor_value * team.sponsors.length) + team.majorSponsor_value + team.factorySponsor_value
+
+    let majorSponsorValue = 0;
+    if(team.majorSponsor){
+        majorSponsorValue = team.majorSponsor_value;
+    }
+
+
+    let sponsorsHTML = `
+        <div id="sponsors-list">
+            <div id="major-sponsor">
+    `;
+    
+    if(team.majorSponsor){
+        sponsorsHTML += `
+            <img src="./img/sponsors/${team.majorSponsor}.webp">
+            <h1>${team.majorSponsor}</h1>
+        `
+    }
+    else {
+        sponsorsHTML += `
+            <img src="./img/sponsors/no_sponsor.webp" onclick="alert('Test')">
+        `
+    }
+
+    sponsorsHTML += `</div><p>${NumberF(majorSponsorValue*1000,"ext",0)}</p>`
+    sponsorsHTML += `<div id="others-sponsors">`
+
+    for(let i = 0; i < 3; i++) {
+        if(i < team.sponsors.length){
+            const sponsor = team.sponsors[i];
+        
+            sponsorsHTML += `
+            <div>
+                <img src="./img/sponsors/${sponsor}.webp">
+                <h1>${sponsor}</h1>
+            </div>
+            `
+        }
+        else {
+            sponsorsHTML += `
+            <div>
+                <img src="./img/sponsors/no_sponsor.webp">
+            </div>
+            `
+        }
+    }
+    sponsorsHTML += `
+        </div>
+        <p>${NumberF(team.sponsor_value*1000,"ext",0)}`
+        
+        if(team.sponsors.length > 1){
+            sponsorsHTML += `
+                cada</p>
+                <p>${NumberF((team.sponsor_value * team.sponsors.length)*1000,"ext",0)} somados</p>
+            `
+        }
+        else {
+            sponsorsHTML += `</p>`
+        }
+
+    sponsorsHTML += `</div>`;
 
     html += `
-        <div id="view-sponsors">
-        </div>`;
-    
+    <div id="sponsors-container">
+        <div id="sponsors">
+            <h2>Patrocinadores</h2>
+            <p>Total: ${NumberF(totalSponsorsValue*1000,"ext",0)} por corrida</p>
+            
+            ${sponsorsHTML}
+        </div>
+        <div>
+            <h2>Metas</h2>
+        </div>
+        <div>
+            <h2>Bônus</h2>
+        </div>
+    `;
+
+    html += `</div>`;
+
     Swal.fire({
         title: `Patrocinadores`,
         html: html,
-        width: "max-content",
+        width: "90%",
         showCloseButton: true,
         focusConfirm: false,
         showConfirmButton: false,
-    }).then(r => {
-        if(returnToMarket)
-            market(scrollPos);
-    });
-}
-
-function negotiate(driverName, returnToMarket){
-    let html = "";
-    const driver = game.drivers[driverName];
-    const team = game.teams[game.team];
-    
-    html += `
-    <div id="negotiate-market">
-        <img id="view-driver-driver-img" src="img/drivers/${driver.image}.webp" onerror="this.onerror=null;this.src='img/drivers/generic.webp';">
-        
-        <div>
-            <div class="slidercontainer">
-                <input id="slider-duration" class="slider" type="range" min="1" value="1" step="1" max="5">
-            </div>
-            <p id="years">1 Ano</p>
-            <br>
-
-            <div class="slidercontainer">
-                <input id="slider-salary" class="slider" type="range" min="${(driver.salary/2)*1000}" step="5" value="${driver.salary*1000}" max="${driver.salary*2000}">
-            </div>
-            <p id="salary">Salário: ${NumberF(driver.salary * 1000000,"ext-short",0)} por corrida</p>
-            <br>
-
-            <p id="salary">Salário Anterior: ${NumberF(driver.salary * 1000000,"ext-short",0)} por corrida</p>
-            <br>
-
-            <select>
-                ${team.new1driver == "" ? "<option>1º Piloto</option>" : ""}
-                ${team.new2driver == "" ? "<option>2º Piloto</option>" : ""}
-                ${team.newTdriver == "" ? "<option>Piloto de Testes</option>" : ""}
-            `
-            if(team.driversAcademy.length < 10 && driver.experience == 0){
-                html += `<option>Piloto da Academia</option>`;
-            }
-            
-            html += `
-            </select>
-            
-            <br>
-            <br>
-            <h4>Aprovação: <span id="approbation">--</span>%</h4>
-        </div>
-    </div>
-    `
-
-    Swal.fire({
-        title: `${driver.name}`,
-        html: html,
-        width: "40em",
-        showCloseButton: false,
-        focusConfirm: true,
-        showConfirmButton: true,
-        showDenyButton: true,
-        confirmButtonText: "Confirmar",
-        denyButtonText: "Cancelar",
-    }).then((result) => {
-        if(result.isConfirmed){
-            if(!document.querySelector("select").value){
-                ;
-            }
-            else{
-                if((Math.random()*100) < approbationCalc(driver.name)){
-                    driver.newTeam = game.team;
-                    driver.newStatus = document.querySelector("select").value;
-                    driver.newSalary = Number(document.querySelector("#slider-salary").value)/1000;
-                    driver.newContractRemainingYears = Number(document.querySelector("#slider-duration").value);
-                    
-                    if(driver.newStatus == "1º Piloto") team.new1driver = driver.name;
-                    if(driver.newStatus == "2º Piloto") team.new2driver = driver.name;
-                    if(driver.newStatus == "Piloto de Testes") team.newTdriver = driver.name;
-
-                    publishNews("Breaking Driver Hire", [game.team, driver.name, driver.newStatus, driver.newContractRemainingYears]);
-
-                    Swal.fire("Contrato Assinado").then(e => {
-                        if(returnToMarket)
-                            market(scrollPos);
-                    });
-                }
-                else{
-                    Swal.fire("Contrato Recusado").then(e => {
-                        game.contractsFailed.push(driver.name);
-
-                        if(returnToMarket)
-                            market(scrollPos);
-                    });
-                }
-                
-                genTeamHTML();
-            }
-        }
-        else if(result.isDenied){
-            viewDriver(driver.name, returnToMarket)
-        }
+    }).then(e => {
+        ;
     });
 
-    approbationCalc(driver.name);
 
-    document.querySelector("select").addEventListener("change", () => {
-        approbationCalc(driver.name);
-    });
-
-    document.querySelector("#slider-duration").addEventListener("input", () => {
-        const sliderValue = Number(document.querySelector("#slider-duration").value);
-
-        document.querySelector("#years").innerHTML = sliderValue == 1 ? sliderValue+" ano" : sliderValue+" anos";
-        approbationCalc(driver.name);
-    });
-
-    document.querySelector("#slider-salary").addEventListener("input", () => {
-        const sliderValue = Number(document.querySelector("#slider-salary").value);
-
-        document.querySelector("#salary").innerHTML = "Salário: "+NumberF(sliderValue * 1000,"ext-short",0)+" por corrida";
-        approbationCalc(driver.name);
-    });
-}
-
-function approbationCalc(driverName){
-    const driver = game.drivers[driverName];
-    const team = game.teams[game.team];
-    let chance;
-
-    if(!document.querySelector("select").innerText){
-        document.querySelector("#approbation").innerText = "--";
-        return;
-    }
-
-    const duration = Number(document.querySelector("#slider-duration").value);
-    const salary = Number(document.querySelector("#slider-salary").value)/1000;
-    const status = document.querySelector("select").value;
-    chance = 70;
-    chance *= (duration * 0.5) / ((driver.experience+10)/100);
-    chance *= Math.pow(salary / getSalary(driver),2);
-
-    if(status == "Piloto da Academia" && driver.age <= driver.careerPeak)
-        chance *= ((duration * 0.5) / ((6-team.reputation) * 500)) * 50;
-
-    if(status == "Piloto de Testes" && driver.age <= driver.careerPeak)
-        chance *= ((duration * 0.5) / ((driver.experience+10) * 5)) * 50;
-
-    if(status == "2º Piloto" && driver.age <= driver.careerPeak && driver.titles > 0)
-        chance *= ((salary * 0.5) / ((driver.experience+10) * 5)) * 50;
-
-    if(status != "Piloto da Academia" && (salary / driver.salary) < 1){
-        chance *=  Math.pow(salary / driver.salary,8);
-    }
-
-    if(chance > 100) chance = 100;
-    if(chance < 5) chance = 0;
-
-    document.querySelector("#approbation").innerText = Math.round(chance);
-    return Math.round(chance);
 }
