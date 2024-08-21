c79dd05938b124f7ec612e859dd5cfab3c19bd78
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2024-08-21 14:59:27 -0300

    feat: editor improvements

--- views/viewEditor.js
+++ views/viewEditor.js
@@ -1,4 +1,8 @@
+import { editTeam } from "../scripts/editor.js";
 import { genID } from "../scripts/utils.js";
+
+export let DB;
+let folderFilepath;
 
 export function viewEditor(){
     let html = "";
@@ -8,6 +12,7 @@ export function viewEditor(){
         <ul>
             <li><button type="button" id="new-db">Novo</button></li>
             <li><button type="button" id="load-db">Carregar</button></li>
+            <li><button type="button" id="save-db" disabled>Salvar</button></li>
         </ul>
         <div id="editor-header-load-file">
             <input type="file" id="editor-file-input" name="file" accept=".json" />
@@ -18,6 +23,29 @@ export function viewEditor(){
 
     document.querySelector("#editor-menu-interface").innerHTML = html;
     
+    document.querySelector("#new-db").addEventListener("click", e => {
+
+        if(confirm("Are you sure?") != true){
+            return;
+        }
+
+        DB = {
+            DB_NAME: "",
+            championship: {
+                teams: [],
+                tracks: [],
+                budgetCap: 135000,
+                pointsSystem: [25,18,15,12,10,8,6,4,2,1],
+                minDriversInTeams: 2,
+                maxDriversInTeams: 2
+            },
+            teams: {},
+            drivers: {}
+        };
+
+        renderDBData();
+    });
+    
     document.querySelector("#load-db").addEventListener("click", e => {
         const fileInputDiv = document.querySelector("#editor-header-load-file");
 
@@ -26,11 +54,16 @@ export function viewEditor(){
         fileInput.click();
         fileInput.addEventListener("change", e => {
             const fileName = e.target.files[0].name;
-    
+            
+            console.log("./db/"+fileName)
+
+            folderFilepath
+
             fetch("./db/"+fileName)
             .then(res => res.json())
-            .then(DB => {
-                renderDBData(DB);
+            .then(loadedDB => {
+                DB = loadedDB
+                renderDBData();
                 fileInputDiv.style.display = "none";
                 fileInput.removeEventListener("change", {});
             })
@@ -58,13 +91,45 @@ export function viewEditor(){
             });
         }*/
     });
+
+
+    document.querySelector("#save-db").addEventListener("click", async e => {
+        const JSONToFile = (filename) => {
+            const blob = new Blob([JSON.stringify(DB, null, 2)], {
+              type: 'application/json',
+            });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = `${filename}.json`;
+            a.click();
+            URL.revokeObjectURL(url);
+        };
+
+        const { value: filename } = await Swal.fire({
+            title: "Filename",
+            input: "text",
+            inputAttributes: {
+              maxlength: "100",
+              autocapitalize: "off",
+              autocorrect: "off"
+            },
+            showCancelButton: false,
+            confirmButtonText: "Save",
+            showLoaderOnConfirm: true
+        })
+
+        if(filename)
+            JSONToFile(filename);
+    });
 }
 
 
-function renderDBData(DB){
+function renderDBData(){
     let html = "";
 
-    console.log(DB)
+    console.log(DB);
+    document.querySelector("#save-db").disabled = false;
 
     let championshipRulesHTML = "";
     for(const key in DB.championship){
@@ -150,7 +215,7 @@ function renderDBData(DB){
 
     document.querySelectorAll(".team-row button").forEach(e => {
         e.addEventListener("click", e => {
-            console.log(e.target.value);
+            renderDBData(editTeam(e.target.innerHTML, DB));
         });
     });
 
