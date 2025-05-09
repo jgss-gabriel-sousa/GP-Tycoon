fe815c544149e57c2fad22c34aadec5bd6117c42
jgss-gabriel-sousa <jgss.gabriel.sousa@gmail.com>
2025-05-09 00:01:40 -0300

    feat: menu team improvement

--- views/menus/menu-team.js
+++ views/menus/menu-team.js
@@ -1,6 +1,7 @@
 import { game } from "../../scripts/game.js";
 import { LOC } from "../../scripts/translation.js";
-import { accentsTidy } from "../../scripts/utils.js";
+import { UpdateDataInfo } from "../../scripts/ui.js";
+import { accentsTidy, NumberF } from "../../scripts/utils.js";
 
 export function initMenuTeam(){
     document.querySelector("#menu-container").innerHTML += `
@@ -50,57 +51,82 @@ export function initMenuTeam(){
 export function updateMenuTeam(){
     genDriversHTML();
     genCarHTML();
+
+    const teams = game.teams;
+    const team = teams[game.team];
+
+    document.querySelector("#team-logo").src = "./img/teams/"+game.team+".png";
+
+    document.querySelector("#year").innerText = `${game.year}`;
+    document.querySelector("#name").innerHTML = `<img class="country-flag" src="img/flags/${accentsTidy(team.country)}.webp"> ${game.team}`;
+    document.querySelector("#money").innerHTML = `<p><img class="icon" src="img/ui/money.png"> ${NumberF(team.cash * 1000,"ext",0)}</p>`;
+    document.querySelector("#supporters").innerHTML = `<p><img class="icon" src="img/ui/supporters.png"> ${NumberF(team.supporters * 1000000,"ext-short",0)}</p>`;
+    
+    let reputationHTML = "<div>"
+    let remainingStars = team.reputation;
+    for(let i = 0; i < 5; i++, remainingStars -= 1) {
+        if(remainingStars > 0 && remainingStars >= 1){
+            reputationHTML += `<span><iconify-icon icon="fa:star"></iconify-icon></span>`;
+        }
+        else if(remainingStars == 0.5){
+            reputationHTML += `<span><iconify-icon icon="fa:star-half-empty"></iconify-icon></span>`;
+        }
+        else{
+            reputationHTML += `<span><iconify-icon icon="fa:star-o"></iconify-icon></span>`;
+        }
+    }
+    reputationHTML += "</div>"
+    document.querySelector("#reputation").innerHTML = reputationHTML;
+
+    
+    if(game.championship.actualRound <= game.championship.tracks.length){
+        const nextRace = game.championship.tracks[game.championship.actualRound-1];
+        let trackStyle;
+
+        if(game.circuits[nextRace].straights > 60) trackStyle = "Retas";
+        else if(game.circuits[nextRace].straights < 40) trackStyle = "Curvas";
+        else trackStyle = "Equilibrado";
+
+        document.querySelector("#next-race-name").innerHTML = `
+        <h2><img class="country-flag" src="img/flags/${accentsTidy(game.circuits[nextRace].country)}.webp">GP ${nextRace}</h2>
+        <img id="next-race-track-img" src="img/ui/tracks/${game.circuits[nextRace].circuit.toLowerCase()}.png" width="70px">
+        <h2>${game.circuits[nextRace].circuit}</h2><small>${trackStyle}</small>`;
+    }
+    else{
+        document.querySelector("#next-race-name").innerHTML = `Resumo da Temporada`;
+    }
 }
 
 
-function genDriversHTML(){
+function genDriversHTML() {
     const el = document.querySelector("#drivers");
+    const team = game.teams[game.team];
     let html = "";
 
-    const team = game.teams[game.team];
-    const driver = [
-        game.drivers[team.driver1],
-        game.drivers[team.driver2],
-    ];
+    const getCareerStage = (driver) => {
+        if (driver.age < driver.careerPeak - 1 && driver.experience < 5) return "Novato";
+        if (driver.age < driver.careerPeak - 1) return "Em Ascensão";
+        if (driver.age <= driver.careerPeak + 1) return "Ápice";
+        return "Veterano";
+    };
 
-    let i = 0;
-    driver.forEach(d => {
-        let careerStage;
-        if(d.age < d.careerPeak-1 && d.experience < 5){
-            careerStage = "Estreante";
+    const renderDriverSectionTitle = (index) => {
+        switch (index) {
+            case 0: return LOC("1st_driver");
+            case 1: return LOC("2nd_driver");
+            case 2: return LOC("test_driver");
+            default: return "";
         }
-        else if(d.age < d.careerPeak-1){
-            careerStage = "Em Ascensão";
-        }
-        else if(d.age > d.careerPeak-1 && d.age < d.careerPeak+2){
-            careerStage = "Ápice";
-        }
-        else{
-            careerStage = "Veterano";
-        }
+    };
 
-        html += `
-        <div class="driver-card">`
-            if(i == 0)
-                html += `<h1>${LOC("1st_driver")}</h1>`
-            if(i == 1)
-                html += `<h1>${LOC("2nd_driver")}</h1>`
-            if(i == 2)
-                html += `<h1>${LOC("test_driver")}</h1>`
-        html += `
-            <img class="driver-card-portrait" src="img/drivers/${d.image}.webp" onerror="this.onerror=null;this.src='img/drivers/generic.webp';">
-            <button class="btn-driver-name view-driver" value="${d.name}">
-                <img class="country-flag" src="img/flags/${accentsTidy(d.country)}.webp"> ${d.name}
-            </button>
-        `
-        
-        html += `
+    const renderStatsTable = (driver, extraRow = "") => {
+        return `
             <table class="bars-table">
                 <tr>
                     <td>${LOC("speed")}:</td>
                     <td>
                         <div class="progress-bar-background">
-                            <div class="progress-bar" style="width:${d.speed}%;"><span>${d.speed}%</span></div>
+                            <div class="progress-bar" style="width:${driver.speed}%;"><span>${driver.speed}%</span></div>
                         </div>
                     </td>
                 </tr>
@@ -108,7 +134,7 @@ function genDriversHTML(){
                     <td>Ritmo:</td>
                     <td>
                         <div class="progress-bar-background">
-                            <div class="progress-bar" style="width:${d.pace}%;"><span>${d.pace}%</span></div>
+                            <div class="progress-bar" style="width:${driver.pace}%;"><span>${driver.pace}%</span></div>
                         </div>
                     </td>
                 </tr>
@@ -116,7 +142,7 @@ function genDriversHTML(){
                     <td>Constância:</td>
                     <td>
                         <div class="progress-bar-background">
-                            <div class="progress-bar" style="width:${d.constancy}%;"><span>${d.constancy}%</span></div>
+                            <div class="progress-bar" style="width:${driver.constancy}%;"><span>${driver.constancy}%</span></div>
                         </div>
                     </td>
                 </tr>
@@ -124,105 +150,97 @@ function genDriversHTML(){
                     <td>Experiência:</td>
                     <td>
                         <div class="progress-bar-background">
-                            <div class="progress-bar" style="width:${d.experience}%;"><span>${d.experience}%</span></div>
+                            <div class="progress-bar" style="width:${driver.experience}%;"><span>${driver.experience}%</span></div>
                         </div>
                     </td>
                 </tr>
                 <tr>
-                    <td>Condição: </td>
-                    <td class="no-border">
-                        ${careerStage}
-                    </td>
+                    <td>Condição:</td>
+                    <td class="no-border">${getCareerStage(driver)}</td>
                 </tr>
+                ${extraRow}
             </table>
-            `
-        
-        if(d.contractRemainingYears == 0 && d.newTeam == ""){
-            html += `<p>Contrato encerrando</p>`
-        }
+        `;
+    };
 
-        html += `</div>`
-        i++;
+    const renderDriverCard = (driver, index) => {
+        return `
+        <div class="driver-card">
+            <h1>${renderDriverSectionTitle(index)}</h1>
+            <img class="driver-card-portrait" src="img/drivers/${driver.image}.webp" onerror="this.onerror=null;this.src='img/drivers/generic.webp';">
+            <button class="btn-driver-name view-driver" value="${driver.name}">
+                <img class="country-flag" src="img/flags/${accentsTidy(driver.country)}.webp"> ${driver.name}
+            </button>
+            ${renderStatsTable(driver)}
+            ${driver.contractRemainingYears === 0 && driver.newTeam === "" ? "<p>Contrato encerrando</p>" : ""}
+        </div>`;
+    };
+
+    // Pilotos principais
+    [team.driver1, team.driver2].forEach((id, index) => {
+        const driver = game.drivers[id];
+        html += renderDriverCard(driver, index);
     });
 
+    // Piloto de testes
     const testDriver = game.drivers[team.test_driver];
-    const ability = Math.round((testDriver.speed + testDriver.pace)/2);
-    let careerStage;
-        if(testDriver.age < testDriver.careerPeak-1 && testDriver.experience < 5){
-            careerStage = "Novato";
-        }
-        else if(testDriver.age < testDriver.careerPeak-1){
-            careerStage = "Em Ascensão";
-        }
-        else if(testDriver.age > testDriver.careerPeak-1 && testDriver.age < testDriver.careerPeak+2){
-            careerStage = "Ápice";
-        }
-        else{
-            careerStage = "Veterano";
-        }
+    const testAbility = Math.round((testDriver.speed + testDriver.pace) / 2);
 
     html += `
     <div class="driver-card">
-    <div id="test-driver">
-        <h1>Piloto de Testes</h1>
-        <button class="btn-driver-name view-driver" value="${testDriver.name}">
+        <div id="test-driver">
+            <h1>Piloto de Testes</h1>
+            <button class="btn-driver-name view-driver" value="${testDriver.name}">
                 <img class="country-flag" src="img/flags/${accentsTidy(testDriver.country)}.webp"> ${testDriver.name}
-        </button>
-        <table class="bars-table">
-            <tr>
-                <td>Hab. Média:</td>
-                <td>
-                    <div class="progress-bar-background">
-                        <div class="progress-bar" style="width:${ability}%;"><span>${ability}%</span></div>
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td>Constância:</td>
-                <td>
-                    <div class="progress-bar-background">
-                        <div class="progress-bar" style="width:${testDriver.constancy}%;"><span>${testDriver.constancy}%</span></div>
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td>Experiência:</td>
-                <td>
-                    <div class="progress-bar-background">
-                        <div class="progress-bar" style="width:${testDriver.experience}%;"><span>${testDriver.experience}%</span></div>
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td>Condição: </td>
-                <td class="no-border">
-                    ${careerStage}
-                </td>
-            </tr>
-        </table>
-    </div>
-    <div id="academy-drivers">
-        <h1>Academia de Pilotos</h1>
-    `
-    
-    team.driversAcademy.forEach(d => {
-        const driver = game.drivers[d];
+            </button>
+            <table class="bars-table">
+                <tr>
+                    <td>Hab. Média:</td>
+                    <td>
+                        <div class="progress-bar-background">
+                            <div class="progress-bar" style="width:${testAbility}%;"><span>${testAbility}%</span></div>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Constância:</td>
+                    <td>
+                        <div class="progress-bar-background">
+                            <div class="progress-bar" style="width:${testDriver.constancy}%;"><span>${testDriver.constancy}%</span></div>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Experiência:</td>
+                    <td>
+                        <div class="progress-bar-background">
+                            <div class="progress-bar" style="width:${testDriver.experience}%;"><span>${testDriver.experience}%</span></div>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Condição:</td>
+                    <td class="no-border">${getCareerStage(testDriver)}</td>
+                </tr>
+            </table>
+        </div>
+    `;
 
+    // Academia de pilotos
+    html += `<div id="academy-drivers"><h1>Academia de Pilotos</h1>`;
+    team.driversAcademy.forEach(id => {
+        const driver = game.drivers[id];
         html += `
-        <button class="btn-driver-name view-driver" value="${driver.name}">
+            <button class="btn-driver-name view-driver" value="${driver.name}">
                 <img class="country-flag" src="img/flags/${accentsTidy(driver.country)}.webp"> ${driver.name}
-        </button>
-        `
+            </button>
+        `;
     });
-    
+    html += `</div></div>`;
 
-    html += `
-    </div>
-    </div>
-    `
-    
     el.innerHTML = html;
-};
+}
+
 
 function genCarHTML(){
     const elCarInfo = document.querySelector("#car-info");
